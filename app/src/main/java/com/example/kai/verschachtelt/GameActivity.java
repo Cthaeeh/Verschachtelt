3ef3712d420a88c8fd0679b2ce4e0cb351aa4a76
Kai <addata@web.de>
2016-07-28 18:15:20 +0200

    Added a GamePanel

--- app/src/main/java/com/example/kai/verschachtelt/GameActivity.java
+++ app/src/main/java/com/example/kai/verschachtelt/GameActivity.java
@@ -13,7 +13,7 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//to be changed
+        setContentView(new GamePanel(this));
 
     }
 
---
+++ app/src/main/java/com/example/kai/verschachtelt/GamePanel.java
@@ -0,0 +1,31 @@
+package com.example.kai.verschachtelt;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Kai on 28.07.2016.
+ */
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+
+    public GamePanel(Context context){
+        super(context);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
