817d5efc1794ee9fe68cd59838e82d42e1216043
Kai <addata@web.de>
2016-07-28 17:47:41 +0200

    Added a GameActivity and a Button that launches the GameActivity.

---
+++ app/src/main/java/com/example/kai/verschachtelt/GameActivity.java
@@ -0,0 +1,20 @@
+package com.example.kai.verschachtelt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kai on 28.07.2016.
+ * This class is for showing the chessBoard and sorrounding UI-Elements
+ *
+ */
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//to be changed
+
+    }
+
+}
--- app/src/main/java/com/example/kai/verschachtelt/MainActivity.java
+++ app/src/main/java/com/example/kai/verschachtelt/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kai.verschachtelt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpUI();
+    }
+
+    private void setUpUI() {
+        //Setup the UI-Elements of The MainActivity
+        Button launchGameActivity = (Button) findViewById(R.id.showGameActivityButton);
+
+        launchGameActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //If the button is pressed launch the Game-Activity
+                Intent startGame = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(startGame);
+            }
+        });
     }
 }
