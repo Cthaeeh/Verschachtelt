47655ea28b95036ed4aafda9a5f2bed1dd304c2c
Kai <addata@web.de>
2016-08-09 15:08:15 +0200

    all chessmen are now displayed. fixed some fps issue by preloading the images of the chessmen

--- app/src/main/java/com/example/kai/verschachtelt/ChessManGraphic.java
+++ app/src/main/java/com/example/kai/verschachtelt/ChessManGraphic.java
@@ -12,12 +12,17 @@ public class ChessManGraphic {
 
     private Paint paint;
     private int canvasWidth,canvasHeight;
-    private Bitmap image;
+    //The images of all chessmen
+    private Bitmap  imageWhiteRook, imageWhiteKnight, imageWhiteBishop, imageWhiteQueen, imageWhiteKing, imageWhitePawn,
+                    imageBlackRook, imageBlackKnight, imageBlackBishop, imageBlackQueen, imageBlackKing, imageBlackPawn;
+    //The length/width of the section containing the chessman to be cropped out. Only applies to the now used image ressource.
+    private final int cropSize = 92;
     private ChessBoard chessBoard;
 
     public ChessManGraphic(Bitmap res){
         paint = new Paint();
-        image = res;
+        extractImages(res);
+
         chessBoard = new ChessBoard();
     }
 
@@ -51,7 +56,10 @@ public class ChessManGraphic {
         int x = stepWidth + (position%8)*stepWidth;
         return x;
     }
-    //Calculate the Y position on the screen where a chessPiece must be drawn.
+
+    /**
+     * Calculate the Y position on the screen where a chessPiece must be drawn.
+     */
     private int getY(int position){
         int stepWidth;                  //The length / width of a square on the board.
         if(canvasHeight<canvasWidth){ stepWidth = canvasHeight/10;
@@ -62,38 +70,38 @@ public class ChessManGraphic {
     }
 
     private Bitmap getChessManImage(ChessBoard.chessMan chessMan){
-        if(chessMan == ChessBoard.chessMan.WHITE_ROOK)
-            return Bitmap.createBitmap(image,1,2,3,4);
-        if(chessMan == ChessBoard.chessMan.WHITE_KNIGHT)
-            return Bitmap.createBitmap(image,1,2,3,4);
-        if(chessMan == ChessBoard.chessMan.WHITE_BISHOP)
-            return Bitmap.createBitmap(image,1,2,3,4);
-        if(chessMan == ChessBoard.chessMan.WHITE_QUEEN)
-            return Bitmap.createBitmap(image,1,2,3,4);
-        if(chessMan == ChessBoard.chessMan.WHITE_KING)
-            return Bitmap.createBitmap(image,1,2,3,4);
-        if(chessMan == ChessBoard.chessMan.WHITE_PAWN)
-            return Bitmap.createBitmap(image,1,2,3,4);
+        if(chessMan == ChessBoard.chessMan.WHITE_ROOK)return imageWhiteRook;
+        if(chessMan == ChessBoard.chessMan.WHITE_KNIGHT)return imageWhiteKnight;
+        if(chessMan == ChessBoard.chessMan.WHITE_BISHOP)return imageWhiteBishop;
+        if(chessMan == ChessBoard.chessMan.WHITE_QUEEN)return imageWhiteQueen;
+        if(chessMan == ChessBoard.chessMan.WHITE_KING)return imageWhiteKing;
+        if(chessMan == ChessBoard.chessMan.WHITE_PAWN)return imageWhitePawn;
         //Black chessman
-        if(chessMan == ChessBoard.chessMan.BLACK_ROOK){
-            return Bitmap.createBitmap(image,174*3,112,88,88);}
-        if(chessMan == ChessBoard.chessMan.BLACK_KNIGHT){
-            return Bitmap.createBitmap(image,174*5,112,88,88);}
-        if(chessMan == ChessBoard.chessMan.BLACK_BISHOP){
-            return Bitmap.createBitmap(image,174*4,112,88,88);}
-        if(chessMan == ChessBoard.chessMan.BLACK_QUEEN){
-            return Bitmap.createBitmap(image,174*2,112,88,88);}
-        if(chessMan == ChessBoard.chessMan.BLACK_KING)
-            return Bitmap.createBitmap(image,0,112,88,88);
-        if(chessMan == ChessBoard.chessMan.BLACK_PAWN)
-            try {
-                return Bitmap.createBitmap(image,960-88,112, 88,88);
-            }catch (Exception e){
-                String output = e.toString();
-                int test = image.getWidth();
-                int x =0;
-            }
-
+        if(chessMan == ChessBoard.chessMan.BLACK_ROOK)  return imageBlackRook;
+        if(chessMan == ChessBoard.chessMan.BLACK_KNIGHT)return imageBlackKnight;
+        if(chessMan == ChessBoard.chessMan.BLACK_BISHOP)return imageBlackBishop;
+        if(chessMan == ChessBoard.chessMan.BLACK_QUEEN) return imageBlackQueen;
+        if(chessMan == ChessBoard.chessMan.BLACK_KING)  return imageBlackKing;
+        if(chessMan == ChessBoard.chessMan.BLACK_PAWN)  return imageBlackPawn;
         return null;
+    }
+
+    /**From a larger image, smaller images of the chessmen are cropped out.
+     * @param imageChessmen
+     */
+    private void extractImages(Bitmap imageChessmen){
+        imageBlackRook = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*2,  112  ,cropSize,cropSize);
+        imageBlackKnight = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*4,112  ,cropSize,cropSize);
+        imageBlackBishop = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*3,112  ,cropSize,cropSize);
+        imageBlackQueen = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*1, 112  ,cropSize,cropSize);
+        imageBlackKing = Bitmap.createBitmap(imageChessmen,0,      112  ,cropSize,cropSize);
+        imageBlackPawn = Bitmap.createBitmap(imageChessmen,960-cropSize , 112  ,cropSize,cropSize);
+
+        imageWhiteRook = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*2,  265  ,cropSize,cropSize);
+        imageWhiteKnight = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*4,265  ,cropSize,cropSize);
+        imageWhiteBishop = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*3,265  ,cropSize,cropSize);
+        imageWhiteQueen = Bitmap.createBitmap(imageChessmen,((960-cropSize)/5)*1, 265  ,cropSize,cropSize);
+        imageWhiteKing = Bitmap.createBitmap(imageChessmen,0,      265  ,cropSize,cropSize);
+        imageWhitePawn = Bitmap.createBitmap(imageChessmen,960-cropSize , 265  ,cropSize,cropSize);
     }
 }
