22fcfd1dca629e757eb4dd95c946ba75071e269a
Kai <addata@web.de>
2016-08-13 21:30:26 +0200

    fixed little Bug (showed in ai mode instantly victoryScreen )

--- app/src/main/java/com/example/kai/verschachtelt/pvAIGame/chess_AI/ChessGamePvAI.java
+++ app/src/main/java/com/example/kai/verschachtelt/pvAIGame/chess_AI/ChessGamePvAI.java
@@ -28,6 +28,7 @@ public class ChessGamePvAI extends ChessGame {
 
     @Override
     public VictoryScreenGraphic.VictoryState getWinner() {
+        if(boardCurrent.getWinner() == null)return null;
         if(boardCurrent.getWinner() != ai.getColor())return VictoryScreenGraphic.VictoryState.VICTORY;
         if(boardCurrent.getWinner() == ai.getColor())return VictoryScreenGraphic.VictoryState.DEFEAT;
         return null;
