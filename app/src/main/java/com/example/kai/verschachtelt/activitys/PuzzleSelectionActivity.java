833f56b209e4a4b242abf2abd8ccf1d8b53eab3c
Kai <addata@web.de>
2016-08-14 13:13:28 +0200

    added a custom action bar in PuzzleSelectionActivity with a refresh button.

--- app/src/main/java/com/example/kai/verschachtelt/activitys/PuzzleSelectionActivity.java
+++ app/src/main/java/com/example/kai/verschachtelt/activitys/PuzzleSelectionActivity.java
@@ -2,6 +2,11 @@ package com.example.kai.verschachtelt.activitys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.ListView;
 
 import com.example.kai.verschachtelt.puzzleGame.PuzzleParser;
@@ -23,17 +28,35 @@ public class PuzzleSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_selection);
+
+        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
         setUpUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        //Use custom actionBar
+        inflater.inflate(R.menu.menu_puzzle_selection, menu);
+        return true;
     }
 
     private void setUpUI() {
         puzzleList = (ListView) findViewById(R.id.puzzleListView);
-
         ArrayList<Puzzle> puzzleArray = new ArrayList<Puzzle>();
         PuzzlesAdapter adapter = new PuzzlesAdapter(this, puzzleArray);
         puzzleList.setAdapter(adapter);
         //Take JSONArray from ressource txt file convert it to Puzzles.
         puzzleArray.addAll(Puzzle.fromJson(PuzzleParser.getJSONArray()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.update_puzzles){
+            //TODO downlaod new puzzles from internet and add them.
+        }
+        return true;
     }
 
 }
